6e65299546909d9217110857dda9a049e0effc92
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-02-06 19:26:11 -0300

    new: docker postgressql configured

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PORT: process.env.POSTGRES_PORT,
+  },
 };
 
 export default nextConfig;
--- src/modules/blog/components/BlogContent.tsx
+++ src/modules/blog/components/BlogContent.tsx
@@ -9,7 +9,7 @@ const BlogContent = () => {
 
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [fetchPosts]);
 
   return <PostList />;
 };
